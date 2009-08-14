d7c6b484682279f0c16eaa1578cafcdf01ea3bc1
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-08-14 19:12:49 +0000

    Some more work on the windows service functions.

--- plugin-install/eg/WinApi/Dynamic.py
+++ plugin-install/eg/WinApi/Dynamic.py
@@ -1482,3 +1482,9 @@ _SERVICE_DESCRIPTIONW._fields_ = [
 ]
 SERVICE_CONFIG_DESCRIPTION = 1 # Variable c_int '1'
 SERVICE_CHANGE_CONFIG = 2 # Variable c_int '2'
+
+GetExitCodeProcess = _kernel32.GetExitCodeProcess
+GetExitCodeProcess.restype = BOOL
+GetExitCodeProcess.argtypes = [HANDLE, LPDWORD]
+ERROR_SERVICE_DOES_NOT_EXIST = 1060 # Variable c_long '1060l'
+
